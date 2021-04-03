afc061dfee706636e0a025f0e08993d9e2567ffe
Dimas Bangkit S <dimasbangkits@gmail.com>
2021-04-04 01:31:25 +0700

    Update eggPriceContracts.ts

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,5 +1,5 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0x380624a4a7e69db1ca07deecf764025fc224d056',
+  cakeAddress: '0xe02df9e3e622debdd69fb838bb799e3f168902c5',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
   lpAddress: '0x0ef564d4f8d6c0ffe13348a32e21efd55e508e84'
 }
