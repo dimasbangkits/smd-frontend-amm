d2245e07c59e5af721e4e030ffca72778eb8b78d
Dimas Bangkit S <dimasbangkits@gmail.com>
2021-04-05 02:27:32 +0700

    a

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -4,32 +4,17 @@ const config: MenuEntry[] = [
   {
     label: 'Home',
     icon: 'HomeIcon',
-    href: 'https://www.goosedefi.com/'
+    href: 'https://smdboys.netlify.app/',
   },
   {
     label: 'Trade',
     icon: 'TradeIcon',
-    initialOpenState: true,
     items: [
       {
         label: 'Exchange',
-        href: '/swap'
+        href: 'https://smdboys.netlify.app/',
       },
-      {
-        label: 'Liquidity',
-        href: '/pool'
-      }
-    ]
-  },
-  {
-    label: 'Farms',
-    icon: 'FarmIcon',
-    href: 'https://www.goosedefi.com/farms'
-  },
-  {
-    label: 'Nests',
-    icon: 'PoolIcon',
-    href: 'https://www.goosedefi.com/nests'
+    ],
   },
 ]
 
